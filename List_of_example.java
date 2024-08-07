88f2a3090f00e669d31e893811ab7f84b4f608e9
MrAnthonyEdwardStark <sureshbabu.innowave@gmail.com>
2024-08-07 08:01:31 +0530

    List.of method to add multiple values in a single line of Code

---
+++ List_of_example.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class List_of_example {
+    public static void main(String[] args) {
+        List<String> list = List.of("Name1","Name2","Name3","Name4");
+        System.out.println(list.toString());
+    }
+}
